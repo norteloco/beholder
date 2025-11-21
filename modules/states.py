84a3bf90fbe52bec0322729a180511a63716a12c
Dmitry <incultx@outlook.com>
2025-11-21 16:01:15 +0300

    fix/unnecessary states have been removed

--- modules/states.py
+++ modules/states.py
@@ -2,8 +2,5 @@ from aiogram.fsm.state import StatesGroup, State
 
 
 class MenuStates(StatesGroup):
-    main_menu = State()  # Main /start
-    repos_menu = State()  # 📃 Repositories
     track_add = State()  # ➕ Add Repository
     track_del = State()  # ➖ Remove Repository
-    track_list = State()  # 📋 List of Repositories
