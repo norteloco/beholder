2511fb577910e9668db9dbfde19861ffb6a576ce
Dmitry <incultx@outlook.com>
2025-11-21 16:03:13 +0300

    fix/accidentally deleted state returned

--- modules/states.py
+++ modules/states.py
@@ -2,5 +2,6 @@ from aiogram.fsm.state import StatesGroup, State
 
 
 class MenuStates(StatesGroup):
+    repos_menu = State()  # 📃 Repositories
     track_add = State()  # ➕ Add Repository
     track_del = State()  # ➖ Remove Repository
